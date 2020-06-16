f9ea89d4fde188f976720c96e268f83c1c82063a
Tanisha-1799 <66749500+Tanisha-1799@users.noreply.github.com>
2020-06-16 13:07:28 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tanisha-simon-game.io
---
+++ js/resp.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed('#typed' , {
+    strings: [
+        'check our memory !!',
+        'get ready to play!!'
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+});
+
+
+
+
